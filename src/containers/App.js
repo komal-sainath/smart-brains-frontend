b4eb768a0dfb16252dca90ea8ef9e008fad6ce30
root <komals1441997@gmail.com>
2019-10-04 12:50:15 +0530

    fix

--- src/containers/App.js
+++ src/containers/App.js
@@ -86,7 +86,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if(response) {
-          fetch('https://regfa-api.herokuapp.com:3001/image', {
+          fetch('https://regfa-api.herokuapp.com/image', {
             method: 'put',
 			      headers: {'Content-Type':'application/json'},
 			      body: JSON.stringify({
